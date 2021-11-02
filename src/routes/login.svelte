66272d2f102b1d2e78f2e4fba4de014febda34c3
giang <giang@LAPTOP-K783C2LT>
2021-11-02 13:29:00 +0100

    fix api route

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -4,7 +4,7 @@
   let email = "", password = "";
 
   const submit = async () => {
-    await fetch('http://localhost:8000/api/register', {
+    await fetch('http://localhost:8000/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
