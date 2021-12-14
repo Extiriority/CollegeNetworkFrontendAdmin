6b20e64531b8a83d0d27b48dc3f1a406ebb5181e
Giang <463189@student.fontys.nl>
2021-12-14 21:57:00 +0100

    add config helper

---
+++ src/helpers/shared/configs.ts
@@ -0,0 +1,13 @@
+export const authConfig = {
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	withCredentials: true
+}
+
+export const baseConfig = {
+	headers: {
+		'content-Type': 'application/x-www-form-urlencoded'
+	},
+	withCredentials: true
+}
