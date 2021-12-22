89d453d1eee446d92637cd6a10a3fbc12af77840
Giang <463189@student.fontys.nl>
2021-12-22 22:38:22 +0100

    UX datepicker and change saved notification

--- src/routes/profile/@[user]/index.svelte
+++ src/routes/profile/@[user]/index.svelte
@@ -4,11 +4,12 @@
 		import rest from '/src/helpers/rest/index.ts';
 		import { authConfig, baseConfig } from '/src/helpers/shared/configs.ts';
 
-		let auth = false;
-    let message
-    let error
+		let auth = false
+		let save = false
+		let checkDate
     let id, firstName, lastName, email, phoneNumber, gender, dateOfBirth, password
 
+		const genders = ['Male', 'Female', 'Other', 'Prefer not to say']
 
 		authenticated.subscribe(a => auth = a);
 
@@ -28,6 +29,10 @@
 								dateOfBirth = new Date(user.dateOfBirth).toISOString().substring(0, 10);
 
 								authenticated.set(true);
+
+								checkDate = new Date('1900-01-01').toISOString().substring(0, 10);
+								if (dateOfBirth < checkDate)
+										dateOfBirth = new Date('2000-01-01').toISOString().substring(0, 10);
 						}
 				}).catch(err => {
 						if (err.response) {
@@ -42,7 +47,9 @@
 				await rest.put('update_user',
 						json,
 						authConfig
-				).catch(err => {
+				).then(() => {
+					save = true
+				}).catch(err => {
 						if (err.response) {
 								console.log(err.response.status);
 								console.log(json)
@@ -104,75 +111,53 @@
 												 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
 												 peer-focus:-top-3 peer-focus:text-gray-600 peer-focus:text-sm">Mobile number</label>
 						</div>
-								<div class='relative py-6'>
-										<h1 class='absolute inline-block mb-4 text-gray-700 -top-1 left-0'
-										>Gender</h1>
-										<div class='flex justify-between'>
+						<div class='relative py-6'>
+								<h1 class='absolute inline-block mb-4 text-gray-700 -top-1 left-0'
+								>Gender</h1>
+								<div class='flex justify-between'>
+										{#each genders as sex}
 												<div class='flex items-center'>
-														<input
-																	name='gender'
-																	type='radio'
-																	class='w-4 h-4 accent-blue-500 border border-gray-300 rounded-full outline-none cursor-pointer'
-																	id='male'
-																	value='Male'
-														/><label class='ml-2 text-sm' for='male'>Male</label>
+														<input value={sex}
+																	 type='radio'
+																	 id={sex}
+																	 bind:group={gender}
+																	 class='w-4 h-4 accent-purple-700 border border-gray-300 rounded-full outline-none cursor-pointer'
+														/><label class='ml-2 text-sm' for={sex}>{sex}</label>
 												</div>
-												<div class='flex items-center'>
-														<input
-																	name='gender'
-																	type='radio'
-																	class='w-4 h-4 accent-pink-500 border border-gray-300 rounded-full outline-none cursor-pointer'
-																	id='female'
-																	value='Female'
-														/><label class='ml-2 text-sm' for='female'>Female</label>
-												</div>
-												<div class='flex items-center'>
-														<input
-																	name='gender'
-																	type='radio'
-																	class='w-4 h-4 accent-gray-500 border border-gray-300 rounded-full outline-none cursor-pointer'
-																	id='other'
-																	value='Other'
-														/><label class='ml-2 text-sm' for='other'>Other</label>
-												</div>
-												<div class='flex items-center'>
-														<input
-																	name='gender'
-																	type='radio'
-																	class='w-4 h-4 accent-red-500 border border-gray-300 rounded-full outline-none cursor-pointer'
-																	checked='checked'
-																	id='private'
-																	value='Private'
-														/><label class='ml-2 text-sm' for='private'>Prefer not to say</label>
-												</div>
-										</div>
+										{/each}
 								</div>
-								<div class='relative'>
-										<input value={dateOfBirth}
-													 type='date'
-													 class="w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-purple-900 pt-3 pb-1 peer placeholder-transparent"
-											     id="floatingDate"
-											     placeholder="Mobile phone number">
-										<label for="floatingDate"
-													 class="absolute left-0 top-0 -top-3.5 text-gray-600 text-base transition-all
-													 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
-													 peer-focus:-top-3 peer-focus:text-gray-600 peer-focus:text-sm">Birthday</label>
-									</div>
-								<div class="relative mt-6">
-										<input bind:value={password}
-													 type="password"
-													 class="w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-purple-900 py-2 peer placeholder-transparent"
-													 id="floatingPassword"
-													 placeholder="Password">
-										<label for="floatingPassword"
-													 class="absolute left-0 top-0 -top-3.5 text-gray-600 text-base transition-all
-													 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
-													 peer-focus:-top-3 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-								</div>
-								<button
-									class='w-full mt-5 py-2 px-4 bg-purple-800 shadow-violet-400 text-white font-semibold rounded-full shadow-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75'
-									type='submit'>Change info
-								</button>
+						</div>
+						<div class='relative'>
+								<input bind:value={dateOfBirth}
+											 type='date'
+											 class="w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-purple-900 pt-3 pb-1 peer placeholder-transparent"
+											 id="floatingDate"
+											 placeholder="Mobile phone number">
+								<label for="floatingDate"
+											 class="absolute left-0 top-0 -top-3.5 text-gray-600 text-base transition-all
+											 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
+											 peer-focus:-top-3 peer-focus:text-gray-600 peer-focus:text-sm">Birthday</label>
+							</div>
+						<div class="relative mt-6">
+								<input bind:value={password}
+											 type="password"
+											 class="w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-purple-900 py-2 peer placeholder-transparent"
+											 id="floatingPassword"
+											 placeholder="Password">
+								<label for="floatingPassword"
+											 class="absolute left-0 top-0 -top-3.5 text-gray-600 text-base transition-all
+											 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
+											 peer-focus:-top-3 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+						</div>
+						<button
+								class='w-full mt-5 py-2 px-4 bg-purple-800 shadow-violet-400 text-white font-semibold rounded-full shadow-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75'
+								type='submit'>Change info
+						</button>
+						{#if save} <!--TODO: make this down right-->
+								 <div class='text-green-700'>
+									 	Changes has been saved
+								 </div>
+						{/if}
 				</form>
 		</div>
 </div>
